f33c18ca601133b6738c053e471192fe77a43a5a
Eduardo Theodoro <eduardotheodorofegit@gmail.com>
2026-08-11 16:02:55 -0300

    chore: env 2

--- src/environments/environment.mobile.ts
+++ src/environments/environment.mobile.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   platform: 'mobile',
-  apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'https://capable-vote-collections-hypothetical.trycloudflare.com/api/v1',
   appName: 'Alicerce Mobile App',
   version: '1.0.0',
 };
