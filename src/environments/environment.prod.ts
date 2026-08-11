d305671df5e6643a75180f0e2bdb3b6a466cfb8c
Eduardo Theodoro <eduardotheodorofegit@gmail.com>
2026-08-11 11:48:54 -0300

    feat-add-environment-prod-for-cloud-api

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  platform: 'web',
+  apiUrl: 'http://localhost:3000/api/v1', // Substituir pela URL publica do seu Backend na nuvem
+  appName: 'Alicerce',
+  version: '1.0.0',
+};
