06219e70e7a9b8aec32bbe47828e6d601aa93a38
Eduardo Theodoro <eduardotheodorofegit@gmail.com>
2026-09-16 21:52:32 -0300

    env

--- src/environments/environment.mobile.ts
+++ src/environments/environment.mobile.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   platform: 'mobile',
-  apiUrl: 'http://localhost:3001/api/v1',
+  apiUrl: 'https://surfaces-interested-aye-selections.trycloudflare.com/api/v1',
   appName: 'Alicerce Mobile App',
   version: '1.0.0',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   platform: 'web',
-  apiUrl: 'http://localhost:3001/api/v1',
+  apiUrl: 'https://surfaces-interested-aye-selections.trycloudflare.com/api/v1',
   appName: 'Alicerce',
   version: '1.0.0',
 };
